850dd1cb9019f5195b5f525fd9d52eb012c3e803
Roman <sayga76239@gmail.com>
2023-01-02 17:25:03 +0100

    sssss

--- js/task-08.js
+++ js/task-08.js
@@ -30,3 +30,27 @@ function onFormSubmit(event) {
 }
 
 
+//----------------------second variant----------------------------
+// const form = document.querySelector('.login-form');
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(e) {
+// e.preventDefault();
+
+// const formElements = e.currentTarget.elements;
+// const mail = formElements.email.value;
+// const password = formElements.password.value;
+
+// if(mail == "" || password == "") {
+//     alert('ALARM!!!');
+// } else {
+//     const formData = {
+//         mail,
+//         password,
+//         };
+//         console.log(formData);
+// } 
+// form.reset();
+// }
+
